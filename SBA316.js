39cf6275739d5f7e4f2dfb36f507a50f026de6c9
dbarker718-ops <dbarker718@gmail.com>
2025-09-17 23:47:29 -0400

    2

---
+++ SBA316.js
@@ -0,0 +1,9 @@
+console.log (document.getElementById("checkbox"))
+console.dir (document.getElementById("checkbox"))
+console.log (document.getElementById("checkbox").checked)
+if (document.getElementById("checkbox").checked==true){
+    console.log("isTrue")
+} 
+else {
+    console.log("isFalse")
+}

5fd9955cfe9bd0f890c1393cf9419a5777650ffe
dbarker718-ops <dbarker718@gmail.com>
2025-09-20 04:20:59 -0400

    newest

--- SBA316.js
+++ SBA316.js
@@ -6,6 +6,18 @@ if (document.getElementById("checkbox").checked == true) {
 } else {
   console.log("isFalse");
 }
+const opt = document.createElement("option");
+const memberForm2 = document.getElementById("Long_Term_Lease");
+const noCheck = document.getElementById("checkbox");
+noCheck.addEventListener("click", function (e) {
+  console.log(e.target.checked);
+  if (e.target.checked) {
+  memberForm.appendChild(opt);}
+});
+const button = document.createElement("button");
+button.id = "button";
+button.innerHTML = "Submit";
+button.type = "submit";
 const user = document.createElement("input");
 user.id = "UserName";
 user.type = "text";
@@ -19,11 +31,13 @@ yesCheck.addEventListener("click", function (e) {
   if (e.target.checked) {
     memberForm.appendChild(user);
     memberForm.appendChild(pass);
+    memberForm.appendChild(button);
     // <input id="UserName" type="text" />
     // <input id="Password" type="text" />
   }
   if (!e.target.checked) {
     user.remove();
     pass.remove();
+    button.remove();
   }
 });
